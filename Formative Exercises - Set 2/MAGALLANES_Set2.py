130542bbb11b82350d082bff9541d90271d45a42
magnus-jx <jaybby.magallanes@student.ateneo.edu>
2024-07-05 09:32:53 +0800

    Update MAGALLANES_Set2.py

--- Formative Exercises - Set 2/MAGALLANES_Set2.py
+++ Formative Exercises - Set 2/MAGALLANES_Set2.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 # Shift Letter Problem
 
 def shift_letter(letter, shift):
@@ -29,47 +35,56 @@ def caesar_cipher(message, shift):
 
 # Shift by Letter Problem
 
-def caesar_cipher(message, shift):
-    message = message.upper()
-    output = ""
-   
-    for char in message:
-        if not char.isalpha():
-            output += char
-            continue
-        shifted_letter = chr(ord("A") + (ord(char) - ord("A") + shift) % 26)
-        output += shifted_letter
-    
-    return output
+def shift_by_letter(letter, letter_shift):
+    if letter == " ":
+        return " "
+    elif not letter.isalpha():
+        return "The input is not an alphabetical letter."
+    else:
+        letter = letter.upper()
+        letter_shift = ord(letter_shift.upper()) - ord("A")
+        for char in letter:
+            if 'A' <= char <= 'Z':
+                shifted_letter = chr(ord("A") + (ord(char) - ord("A") + letter_shift) % 26)
+        return shifted_letter
 
 # Vigenere Cipher Problem
 
-def caesar_cipher(message, shift):
+def vigenere_cipher(message, key):
     message = message.upper()
+    key = key.upper()
     output = ""
-   
-    for char in message:
-        if not char.isalpha():
-            output += char
-            continue
-        shifted_letter = chr(ord("A") + (ord(char) - ord("A") + shift) % 26)
-        output += shifted_letter
+    key_index = 0
+    key_length = len(key)
     
+    for real_char in message:
+        if not real_char.isalpha():
+            output += real_char
+            key_index = (key_index + 1) % key_length
+            continue
+        transformed_char = chr(ord("A") + (ord(real_char) + ord(key[key_index]) - (2 * ord("A"))) % 26)
+        output += transformed_char
+        key_index = (key_index + 1) % key_length
+        
     return output
 
 # Scytale Cipher Problem
 
-def caesar_cipher(message, shift):
+def scytale_cipher(message, shift):
     message = message.upper()
+    message_index = 0
+    message_length = len(message)
     output = ""
-   
-    for char in message:
-        if not char.isalpha():
-            output += char
-            continue
-        shifted_letter = chr(ord("A") + (ord(char) - ord("A") + shift) % 26)
-        output += shifted_letter
     
+    while message_length % shift != 0:
+        message += "_"
+        message_length = len(message)
+    
+    while message_index < message_length:
+        transformed_char = message[((message_index // shift) + (message_length // shift) * (message_index % shift))]
+        output += transformed_char
+        message_index += 1
+        
     return output
 
 # Syctale Decipher Problem
